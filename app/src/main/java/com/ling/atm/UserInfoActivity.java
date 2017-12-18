9343861bf863121b2cdd5993f676d5781f0995da
YulingChang <aa0932299020@gmail.com>
2017-12-18 23:47:52 +0800

    Update UserInfoActivity.java

--- app/src/main/java/com/ling/atm/UserInfoActivity.java
+++ app/src/main/java/com/ling/atm/UserInfoActivity.java
@@ -6,15 +6,22 @@ import android.view.View;
 import android.widget.EditText;
 
 public class UserInfoActivity extends AppCompatActivity {
+    private EditText edNickname;
+    private EditText edTel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-    }
-    public void back(View view){
         EditText edNickname = (EditText) findViewById(R.id.nickname);
         EditText edTel = (EditText) findViewById(R.id.tel);
+        String nickname = getSharedPreferences("info", MODE_PRIVATE).getString("NAME","");
+        String tel = getSharedPreferences("info", MODE_PRIVATE).getString("PHONE","");
+        edNickname.setText(nickname);
+        edTel.setText(tel);
+    }
+    public void back(View view){
+       
         String nickname = edNickname.getText().toString();
         String tel = edTel.getText().toString();
         getIntent().putExtra("EXTRA_USERNICKNAME",nickname);
