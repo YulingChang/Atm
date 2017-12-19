29ba7ccba01bb25164847896250c73e09e038fed
Yuling <aa0932299020@gmail.com>
2017-12-19 11:46:41 +0800

    ch8

--- app/src/main/java/com/ling/atm/UserInfoActivity.java
+++ app/src/main/java/com/ling/atm/UserInfoActivity.java
@@ -2,12 +2,19 @@ package com.ling.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
     private EditText edNickname;
     private EditText edTel;
+    private static final String TAG = UserInfoActivity.class.getSimpleName();
+    private Spinner ages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,19 @@ public class UserInfoActivity extends AppCompatActivity {
         String tel = getSharedPreferences("info", MODE_PRIVATE).getString("PHONE","");
         edNickname.setText(nickname);
         edTel.setText(tel);
+        ages = (Spinner) findViewById(R.id.ages);
+       /* ArrayList<String> data = new ArrayList<>();
+        for(int i = 15;i<=40;i++){
+            data.add(i+"");
+        }*/
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ages, android.R.layout.simple_list_item_1);
+        ages.setAdapter(adapter);
+
     }
-    public void back(View view){
+    public void ok(View view){
+        Log.d(TAG,"ok:" + ages.getSelectedItem().toString());
+        int age = Integer.parseInt(ages.getSelectedItem().toString());
         String nickname = edNickname.getText().toString();
         String tel = edTel.getText().toString();
         getIntent().putExtra("EXTRA_USERNICKNAME",nickname);
