ab46902e7d7353cc7e19c6bbaaca7a6e8c71d776
Yuling <aa0932299020@gmail.com>
2018-01-05 01:13:46 +0800

    2018/1/4

--- app/src/main/java/com/ling/atm/AreaActivity.java
+++ app/src/main/java/com/ling/atm/AreaActivity.java
@@ -22,22 +22,22 @@ public class AreaActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_area);
         Intent p = getIntent();
         String position = p.getStringExtra("area");
-        areas = (ListView)findViewById(R.id.areas);
-            if (position.equals("基隆市")){
-                area1 = new String[]{"中正區","暖暖區","八堵區"};
-                ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,area1);
-                areas.setAdapter(adapter);
-    }else if (position.equals("新北市")){
-                area2 = new String[]{"永和區","板橋區","新莊區"};
-                ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,area2);
-                areas.setAdapter(adapter);
-            }else if (position.equals("台北市")){
-                area2 = new String[]{"信義區","大安區","士林區"};
-                ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,area3);
-                areas.setAdapter(adapter);
-}
-areas.setOnItemClickListener(this);
-
+        areas = (ListView) findViewById(R.id.areas);
+        if (position.equals("基隆市")) {
+            area1 = new String[]{"中正區", "暖暖區", "八堵區"};
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, area1);
+            areas.setAdapter(adapter);
+        } else if (position.equals("新北市")) {
+            area2 = new String[]{"永和區", "板橋區", "新莊區"};
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, area2);
+            areas.setAdapter(adapter);
+        } else if (position.equals("台北市")) {
+            area2 = new String[]{"信義區", "大安區", "士林區"};
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, area3);
+            areas.setAdapter(adapter);
+        }
+        areas.setOnItemClickListener(this);
+    }
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
             getIntent().putExtra("Area", area1[position]);
@@ -45,6 +45,6 @@ areas.setOnItemClickListener(this);
             getIntent().putExtra("Area", area3[position]);
             setResult(RESULT_OK,getIntent());
             finish();
-        }}
+        }
 
     }
--- app/src/main/java/com/ling/atm/CityActivity.java
+++ app/src/main/java/com/ling/atm/CityActivity.java
@@ -1,13 +1,43 @@
 package com.ling.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class CityActivity extends AppCompatActivity {
+public class CityActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView city;
+    private String[] cities;
+    public static final String TAG = CityActivity.class.getSimpleName();
+    public static final int REQUEST_LOGIN = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
+        city = (ListView) findViewById(R.id.city);
+        cities = new String[]{"基隆市","新北市","台北市"};
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,cities);
+        city.setAdapter(adapter);
+        city.setOnItemClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        Log.d(TAG, "onItemClick:  "+ position + "/" + cities[position]);
+        Intent p = new Intent(this, AreaActivity.class);
+        p.putExtra("area", cities[position] );
+        startActivityForResult(p, REQUEST_LOGIN);
+
     }
 }
